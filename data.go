71a93cb7ce26afe9e2531c4e9787bbff5733a539
aYaN21406 <74656720+aYaN21406@users.noreply.github.com>
2025-11-27 22:21:47 -0500

    Add sample job dataset (data.go)

---
+++ data.go
@@ -0,0 +1,20 @@
+package main
+
+// Jobs is the sample dataset of occupations with risk scores
+var Jobs = []Job{
+	{"Cashier", 0.95, 0.1, 0.1, 0.2, 0.9},
+	{"Data Entry Clerk", 0.92, 0.05, 0.1, 0.3, 0.85},
+	{"Telemarketer", 0.88, 0.05, 0.15, 0.25, 0.8},
+	{"Bookkeeper", 0.85, 0.05, 0.2, 0.4, 0.75},
+	{"Receptionist", 0.78, 0.1, 0.25, 0.35, 0.7},
+	{"Truck Driver", 0.7, 0.8, 0.15, 0.3, 0.6},
+	{"Accountant", 0.65, 0.05, 0.35, 0.6, 0.55},
+	{"Software Developer", 0.3, 0.05, 0.85, 0.9, 0.4},
+	{"Graphic Designer", 0.35, 0.1, 0.9, 0.6, 0.45},
+	{"Nurse", 0.25, 0.7, 0.6, 0.7, 0.3},
+	{"Physical Therapist", 0.2, 0.85, 0.65, 0.75, 0.25},
+	{"Teacher", 0.35, 0.3, 0.8, 0.85, 0.35},
+	{"Lawyer", 0.4, 0.05, 0.75, 0.9, 0.4},
+	{"Doctor", 0.2, 0.5, 0.85, 0.95, 0.2},
+	{"Electrician", 0.3, 0.85, 0.5, 0.6, 0.35},
+}
